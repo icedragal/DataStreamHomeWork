accb0e085ee988f26f14623260b3f8b336221484
Icedragal <icedragal@gmail.com>
2023-01-23 13:08:46 +0300

    Initial commit

---
+++ src/main/java/me/egorzhuravlev/datastreamhomework/DataStreamHomeWorkApplication.java
@@ -0,0 +1,13 @@
+package me.egorzhuravlev.datastreamhomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataStreamHomeWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataStreamHomeWorkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/egorzhuravlev/datastreamhomework/DataStreamHomeWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package me.egorzhuravlev.datastreamhomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataStreamHomeWorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
